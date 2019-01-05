1f54930b637bc6e1b24e39e22583d20a04fd8577
Naser Sobhan <nasersobhan@outlook.com>
2019-01-05 15:44:02 +0430

    init

---
+++ renamerv1/Form1.Designer.vb
@@ -0,0 +1,244 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.pathtxt = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.resulttxt = New System.Windows.Forms.TextBox()
+        Me.pbar1 = New System.Windows.Forms.ProgressBar()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.toLowerchk = New System.Windows.Forms.CheckBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.addchar = New System.Windows.Forms.TextBox()
+        Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'pathtxt
+        '
+        Me.pathtxt.Location = New System.Drawing.Point(38, 44)
+        Me.pathtxt.Name = "pathtxt"
+        Me.pathtxt.Size = New System.Drawing.Size(348, 20)
+        Me.pathtxt.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(389, 43)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Start Renaming"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(6, 19)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(127, 69)
+        Me.ListBox1.TabIndex = 2
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(58, 92)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(35, 23)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "Add"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(9, 27)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(29, 13)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Path"
+        '
+        'resulttxt
+        '
+        Me.resulttxt.Location = New System.Drawing.Point(12, 216)
+        Me.resulttxt.Multiline = True
+        Me.resulttxt.Name = "resulttxt"
+        Me.resulttxt.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal
+        Me.resulttxt.Size = New System.Drawing.Size(461, 165)
+        Me.resulttxt.TabIndex = 7
+        '
+        'pbar1
+        '
+        Me.pbar1.Location = New System.Drawing.Point(12, 387)
+        Me.pbar1.Name = "pbar1"
+        Me.pbar1.Size = New System.Drawing.Size(461, 24)
+        Me.pbar1.TabIndex = 8
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(99, 92)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(34, 23)
+        Me.Button3.TabIndex = 9
+        Me.Button3.Text = "Remove Selected"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.TextBox2)
+        Me.GroupBox1.Controls.Add(Me.TextBox1)
+        Me.GroupBox1.Controls.Add(Me.toLowerchk)
+        Me.GroupBox1.Location = New System.Drawing.Point(159, 88)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(314, 122)
+        Me.GroupBox1.TabIndex = 10
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Rename Options"
+        '
+        'toLowerchk
+        '
+        Me.toLowerchk.AutoSize = True
+        Me.toLowerchk.Location = New System.Drawing.Point(18, 32)
+        Me.toLowerchk.Name = "toLowerchk"
+        Me.toLowerchk.Size = New System.Drawing.Size(94, 17)
+        Me.toLowerchk.TabIndex = 0
+        Me.toLowerchk.Text = "To Lowercase"
+        Me.toLowerchk.UseVisualStyleBackColor = True
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.addchar)
+        Me.GroupBox2.Controls.Add(Me.ListBox1)
+        Me.GroupBox2.Controls.Add(Me.Button3)
+        Me.GroupBox2.Controls.Add(Me.Button2)
+        Me.GroupBox2.Location = New System.Drawing.Point(12, 88)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(141, 122)
+        Me.GroupBox2.TabIndex = 11
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Chars to replace with -"
+        '
+        'addchar
+        '
+        Me.addchar.Location = New System.Drawing.Point(6, 92)
+        Me.addchar.Name = "addchar"
+        Me.addchar.Size = New System.Drawing.Size(46, 20)
+        Me.addchar.TabIndex = 10
+        '
+        'FolderBrowserDialog1
+        '
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(11, 43)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(26, 23)
+        Me.Button4.TabIndex = 12
+        Me.Button4.Text = "..."
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(60, 52)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(65, 20)
+        Me.TextBox1.TabIndex = 1
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(179, 52)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(65, 20)
+        Me.TextBox2.TabIndex = 1
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(15, 55)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(33, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Prefix"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(140, 55)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(28, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "sufix"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(491, 426)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.pbar1)
+        Me.Controls.Add(Me.resulttxt)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.pathtxt)
+        Me.Name = "Form1"
+        Me.Text = "Auto-Renamer"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents pathtxt As System.Windows.Forms.TextBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents resulttxt As System.Windows.Forms.TextBox
+    Friend WithEvents pbar1 As System.Windows.Forms.ProgressBar
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents toLowerchk As System.Windows.Forms.CheckBox
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents FolderBrowserDialog1 As System.Windows.Forms.FolderBrowserDialog
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents addchar As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+
+End Class
---
+++ renamerv1/Form1.vb
@@ -0,0 +1,143 @@
+﻿Imports System.IO
+
+Public Class Form1
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        If Not pathtxt.Text = "" Then
+
+            My.Settings.xpath = pathtxt.Text
+            My.Settings.Save()
+
+            Dim sourcePath As String = pathtxt.Text
+
+
+
+            Dim searchPattern As String = "*.*"
+            Dim i As Integer = 0
+            resulttxt.Text = "Start Renameing..."
+            Dim files As Array = Directory.GetFiles(sourcePath, searchPattern, SearchOption.AllDirectories)
+            pbar1.Visible = True
+            ' Set Minimum to 1 to represent the first file being copied.
+            pbar1.Minimum = 1
+            ' Set Maximum to the total number of files to copy.
+            pbar1.Maximum = files.Length
+            ' Set the initial value of the ProgressBar.
+            pbar1.Value = 1
+            ' Set the Step property to a value of 1 to represent each file being copied.
+            pbar1.Step = 1
+
+            '   For Each folderName As String In Directory.GetDirectories(sourcePath, searchPattern, SearchOption.AllDirectories)
+
+
+            For Each fileName As String In files
+
+
+
+
+
+                Dim basenamex As String = Path.GetFileName(fileName)
+                ' Dim ext As String = Path.GetExtension(fileName)
+
+
+
+                File.Move(Path.Combine(sourcePath, fileName), Path.Combine(sourcePath, fileName.Replace(basenamex, renameer(basenamex))))
+
+
+                resulttxt.Text = resulttxt.Text & vbCrLf & "Renamed: {" & basenamex & "} to {" & renameer(basenamex) & "}"
+
+                i += 1
+                pbar1.PerformStep()
+            Next
+            resulttxt.Text = resulttxt.Text & vbCrLf & "Stop task..."
+
+            '   Next
+        Else
+            MsgBox("Please Insert path")
+            pathtxt.Focus()
+
+        End If
+
+    End Sub
+
+
+    Function renameer(ByVal fname As String)
+        Dim newfname As String = fname
+        ' For Each st As String In ListBox1.SelectedItems
+        'newfname = newfname.Replace("", "-")
+        ' Next
+       
+        If toLowerchk.Checked Then
+            newfname = newfname.ToLower()
+        End If
+
+        For l_index As Integer = 0 To ListBox1.Items.Count - 1
+          
+            Dim l_text As String = CStr(ListBox1.Items(l_index))
+            newfname = newfname.Replace(l_text, "-")
+
+        Next
+
+
+
+        Return newfname
+    End Function
+
+    Private Sub Form1_FormClosing(ByVal sender As Object, ByVal e As System.Windows.Forms.FormClosingEventArgs) Handles Me.FormClosing
+        My.Settings.chars = " "
+
+
+        For l_index As Integer = 0 To ListBox1.Items.Count - 1
+
+            Dim l_text As String = CStr(ListBox1.Items(l_index))
+
+            My.Settings.chars = My.Settings.chars & "-" & l_text
+            My.Settings.Save()
+        Next
+    End Sub
+
+ 
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        pathtxt.Text = My.Settings.xpath
+     
+
+        Dim strarr() As String
+        strarr = My.Settings.chars.Split("-")
+
+        For Each s As String In strarr.Distinct()
+            ListBox1.Items.Add(s)
+        Next
+
+        'addchar.Text = My.Settings.chars
+
+    End Sub
+
+    Private Sub Button4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button4.Click
+        FolderBrowserDialog1.ShowDialog()
+        pathtxt.Text = FolderBrowserDialog1.SelectedPath
+    End Sub
+
+    Private Sub FolderBrowserDialog1_HelpRequest(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles FolderBrowserDialog1.HelpRequest
+
+    End Sub
+
+    Private Sub Label1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        If Not addchar.Text = "" Then
+            ListBox1.Items.Add(addchar.Text)
+        Else
+            MsgBox("type something")
+        End If
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        ListBox1.Items.Remove(ListBox1.SelectedItem)
+    End Sub
+
+    Private Sub Label2_Click(sender As System.Object, e As System.EventArgs) Handles Label2.Click, Label3.Click
+
+    End Sub
+End Class
